b72cd6a7862037ae59b16373ec44c64aa4d24a09
Phil Crump <phildcrump@gmail.com>
2014-05-22 14:22:55 +0100

    arduino_sensor: Use ignored NodeConfig.

--- arduino_sensor/arduino_sensor.ino
+++ arduino_sensor/arduino_sensor.ino
@@ -12,53 +12,18 @@ Based on UKHASnet rf69_repeater by James Coxon M6JCX
 #include "RFM69.h"
 #include "LowPower.h"
 #include "DallasTemperature.h"
-
-
-#define PS02
-
-//************* Node ID Setup ****************/
-#ifdef PS00
-char id[] = "PS00";
-char location_string[] = "50.93891,-1.39774";
-#define BATTV_FUDGE 1.109
-#define BEACON_INTERVAL 15
-uint8_t rfm_power = 10; // dBmW
-byte num_repeats = '0'; //The number of hops the message will make before stopping
-#endif
-
-#ifdef PS01
-char id[] = "PS01";
-char location_string[] = "50.93898,-1.39762";
-#define BATTV_FUDGE 1.109
-#define BEACON_INTERVAL 15
-uint8_t rfm_power = 10; // dBmW
-byte num_repeats = '0'; //The number of hops the message will make before stopping
-#endif
-
-#ifdef PS02
-char id[] = "PS02";
-char location_string[] = "50.93877,-1.3979";
-#define BATTV_FUDGE 1.109
-#define BEACON_INTERVAL 15
-uint8_t rfm_power = 10; // dBmW
-byte num_repeats = '0'; //The number of hops the message will make before stopping
-#endif
-
-//************* Sensors ****************/
-// Battery Voltage Measurement - Also enables zombie mode
-#define ENABLE_BATTV_SENSOR // Comment out to disable, also disables zombie mode
-#define BATTV_PIN 0 //ADC 0 - Battery Voltage, scaled to 1.1V
+#include "NodeConfig.h"
 
 //************* Misc Setup ****************/
 DeviceAddress ds_addr;
 float battV=0.0;
 uint8_t n;
-uint32_t count = 1, data_interval = 10;
+uint32_t count = 1, data_interval = 2;
 uint8_t data_count = 97; // 'a'
 char data[64], string_end[] = "]";
 
 // Singleton instance of the radio
-RFM69 rf69(9.3); // parameter: RFM temperature calibration offset (degrees as float)
+RFM69 rf69(RFM_TEMP_FUDGE); // parameter: RFM temperature calibration offset (degrees as float)
 OneWire ow(9);  // on pin PB1 (arduino: 9)
 DallasTemperature sensors(&ow);
 
