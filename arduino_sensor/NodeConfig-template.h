3640d3e025cd7f460c5793cca60eddf4b40e102f
Phil Crump <phildcrump@gmail.com>
2014-10-08 01:00:08 +0100

    arduino_sensor: Reconfigured for DHT22 option.

--- arduino_sensor/NodeConfig-template.h
+++ arduino_sensor/NodeConfig-template.h
@@ -1,35 +1,29 @@
 //************* Define Node that you're compiling for ****************/
-#define PS00
+#define ZZ00
 
 //************* Node-specific config ****************/
-#ifdef PS00
-char id[] = "PS00";
-char location_string[] = "50.93889,-1.39782";
+#ifdef XX00
+char id[] = "XX00";
+#define LOCATION_STRING "50.3475,-1.33445"
 byte num_repeats = '1'; //The number of hops the message will make in the network
 #define BATTV_FUDGE 1.109 // Battery Voltage ADC Calibration
 #define BEACON_INTERVAL 15 // Beacon Interval is (x * 8) seconds
 uint8_t rfm_power = 20; // dBmW
+#define DS18B20
+#define ENABLE_BATTV_SENSOR // Comment out to disable
 #endif
 
-#ifdef PS01
-char id[] = "PS01";
-char location_string[] = "50.93805,-1.39729";
+#ifdef YY01
+char id[] = "YY01";
+#define LOCATION_STRING "50.9346,-1.4531"
 byte num_repeats = '1'; //The number of hops the message will make in the network
 #define BATTV_FUDGE 1.109 // Battery Voltage ADC Calibration
 #define BEACON_INTERVAL 15 // Beacon Interval is (x * 8) seconds
 uint8_t rfm_power = 10; // dBmW
-#endif
-
-#ifdef PS02
-char id[] = "PS02";
-char location_string[] = "50.93877,-1.3979";
-byte num_repeats = '1'; //The number of hops the message will make in the network
-#define BATTV_FUDGE 1.109 // Battery Voltage ADC Calibration
-#define BEACON_INTERVAL 15 // Beacon Interval is (x * 8) seconds
-uint8_t rfm_power = 10; // dBmW
+#define DHT22
+#define ENABLE_BATTV_SENSOR // Comment out to disable
 #endif
 
 //************* Other config ****************/
-#define ENABLE_BATTV_SENSOR // Comment out to disable
 #define BATTV_PIN 0 //ADC 0 - Battery Voltage, scaled to 1.1V
 #define RFM_TEMP_FUDGE 0
--- arduino_sensor/arduino_sensor.ino
+++ arduino_sensor/arduino_sensor.ino
@@ -11,11 +11,23 @@ Based on UKHASnet rf69_repeater by James Coxon M6JCX
 #include "RFM69Config.h"
 #include "RFM69.h"
 #include "LowPower.h"
-#include "DallasTemperature.h"
+
 #include "NodeConfig.h"
 
-//************* Misc Setup ****************/
+#ifdef DS18B20
+#include "DallasTemperature.h"
 DeviceAddress ds_addr;
+OneWire ow(9);  // on pin PB1 (arduino: 9)
+DallasTemperature sensors(&ow);
+#endif
+
+#ifdef DHT22
+#include "dht.h"
+dht DHT;
+#define DHT22_PIN 9 // Arduino pin 9
+#endif
+
+//************* Misc Setup ****************/
 float battV=0.0;
 uint8_t n;
 uint32_t count = 1, data_interval = 2;
@@ -24,8 +36,7 @@ char data[64], string_end[] = "]";
 
 // Singleton instance of the radio
 RFM69 rf69(RFM_TEMP_FUDGE); // parameter: RFM temperature calibration offset (degrees as float)
-OneWire ow(9);  // on pin PB1 (arduino: 9)
-DallasTemperature sensors(&ow);
+
 
 #ifdef ENABLE_BATTV_SENSOR
 float sampleBattv() {
@@ -35,18 +46,38 @@ float sampleBattv() {
 #endif
 
 int gen_Data(){
+
+  #ifdef LOCATION_STRING
   if(data_count=='a' or data_count=='z') {
-      sprintf(data, "%c%cL%s", num_repeats, data_count, location_string);
+      sprintf(data, "%c%cL%s", num_repeats, data_count, LOCATION_STRING);
   } else {
       sprintf(data, "%c%c", num_repeats, data_count);
   }
+  #else
+  sprintf(data, "%c%c", num_repeats, data_count);
+  #endif
   
+  // Temperature (DS18B20 or DHT22)
+  
+  #ifdef DS18B20
   sensors.setWaitForConversion(false);
   sensors.requestTemperatures();
   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
   
   float temp = sensors.getTempC(ds_addr);
+  #endif
+  
+  #ifdef DHT22
+  int chk = DHT.read22(DHT22_PIN);
 
+  while(chk!=DHTLIB_OK)
+  {
+    LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+    chk = DHT.read22(DHT22_PIN);
+  }
+  float temp = DHT.temperature;
+  #endif
+  
   char* tempStr;
   char tempStrA[12]; //make buffer large enough for 7 digits
   tempStr = dtostrf(temp,6,1,tempStrA);
@@ -55,6 +86,21 @@ int gen_Data(){
      strcpy(tempStr,&tempStr[1]);
   }
   sprintf(data,"%sT%s",data,tempStr);
+  
+  // Humidity (DHT22)
+  
+  #ifdef DHT22
+  float humid = DHT.humidity;
+  
+  tempStr = dtostrf(humid,6,1,tempStrA);
+  while( (strlen(tempStr) > 0) && (tempStr[0] == 32) )
+  {
+     strcpy(tempStr,&tempStr[1]);
+  }
+  sprintf(data,"%sH%s",data,tempStr);
+  #endif
+  
+  // Battery Voltage
   
   #ifdef ENABLE_BATTV_SENSOR
   battV = sampleBattv();
@@ -77,9 +123,11 @@ void setup()
   randomSeed(analogRead(6));
   delay(1000);
   
+  #ifdef DS18B20
   sensors.begin();
   sensors.getAddress(ds_addr, 0);
   sensors.setResolution(ds_addr, 12);
+  #endif
   
   while (!rf69.init()){
     delay(100);
