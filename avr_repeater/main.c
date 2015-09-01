17ef3ca599dea8ea9c9338b08cb95f8b9001375c
Jon Sowman <jon@jonsowman.com>
2015-09-01 14:13:52 +0100

    Tidying up avr_repeater a bit more

--- avr_repeater/main.c
+++ avr_repeater/main.c
@@ -18,7 +18,6 @@
 #include <util/delay.h>
 
 #include "ukhasnet-rfm69.h"
-#include "ukhasnet-rfm69-config.h"
 #include "nodeconfig.h"
 
 /* Zombie mode */
@@ -44,87 +43,6 @@ void loop(void);
 void sendPacket(void);
 
 /**
- * Measure the battery voltage as measured by a potential divider on BATTV_PIN
- * defined in nodeconfig.h. BATTV_SCALEFACTOR should also be set appropriately
- * in this file.
- * @returns The voltage of the battery in Volts
- */
-float get_batt_voltage(void)
-{
-    uint16_t res;
-
-    /* Power up the ADC */
-    PRR &= ~(_BV(PRADC));
-
-    /* Set channel to PC0 */
-    ADMUX = BATTV_PIN;
-
-    /* Set internal 1V1 reference */
-    ADMUX |= _BV(REFS1) | _BV(REFS0);
-
-    /* Get 125kHz ADC clock from a 1MHz core clock by dividing by 8 */
-    ADCSRA |= _BV(ADPS1) | _BV(ADPS0);
-
-    /* Enable ADC */
-    ADCSRA |= _BV(ADEN);
-
-    /* Start conv, wait until done, get result */
-    ADCSRA |= _BV(ADSC);
-    while(!(ADCSRA & _BV(ADIF)));
-    res = ADC;
-
-    /* Power down the ADC */
-    ADCSRA &= ~_BV(ADEN);
-    PRR |= _BV(PRADC);
-
-    return (float)(res * 0.001074219 * BATTV_SCALEFACTOR * BATTV_FUDGE);
-}
-
-/**
- * Given a char buffer, generate a UKHASnet packet and return the length of
- * the packet written to the supplied buffer.
- * @param buf The char buffer into which to write the packet
- * @returns The length of the packet that was written
- */
-int16_t gen_data(char *buf)
-{
-    int8_t temp = 0;
-
-#ifdef LOCATION_STRING
-    if(sequence_id=='a' || sequence_id=='z') {
-        sprintf(buf, "%u%cL%s", NUM_REPEATS, sequence_id, LOCATION_STRING);
-    } else {
-        sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
-    }
-#else
-    sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
-#endif
-
-    rf69_readTemp(&temp);
-    sprintf(buf, "%sT%i.0", buf, temp);
-
-    // Battery Voltage
-#if ENABLE_BATTV_SENSOR == 1
-    battV = get_batt_voltage();
-    char* battStr;
-    char tempStrB[14]; //make buffer large enough for 7 digits
-    battStr = dtostrf(battV,7,2,tempStrB);
-    while( (strlen(battStr) > 0) && (battStr[0] == 32) )
-    {
-        strcpy(battStr,&battStr[1]);
-    }
-    sprintf(buf, "%sV%s", buf, battStr);
-#endif
-
-    // If zombie mode is enabled, put the current zombie status in telem
-#ifdef ENABLE_ZOMBIE_MODE
-    sprintf(buf, "%sZ%d", buf, zombie_mode);
-#endif /* ENABLE_ZOMBIE_MODE */
-
-    return sprintf(buf, "%s[%s]", buf, NODE_ID);
-}
-
-/**
  * Initialise internal and external peripherals, including reading config and
  * setting power savings modes.
  */
@@ -135,6 +53,8 @@ void init(void)
 
     while(rf69_init() != RFM_OK)
         _delay_ms(100);
+
+    /* Transmit an initial packet */
     sendPacket();
 }
 
@@ -212,7 +132,8 @@ int main(void)
 void loop(void)
 {
     bool ispacket;
-    uint8_t i;
+    uint8_t i, j, k, start_bracket, end_bracket;
+    uint16_t delaytime;
 
     count++;
     wdt_reset();
@@ -220,30 +141,26 @@ void loop(void)
     if( zombie_mode == MODE_NORMAL )
     {
         rf69_setMode(RFM69_MODE_RX);
+
+        /* This loop is calculated to take roughly 8 seconds, i.e. 255
+         * iterations with a 30ms delay */
         for(i = 0; i < 255; i++) {
             wdt_reset();
             //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, 
             //  TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
             _delay_ms(30);
             rf69_receive(buf, &len, &lastrssi, &ispacket);
-            if(ispacket)
+
+            /* The boolean variable 'ispacket' will be true if rf69_receive
+             * tells us there is a packet waiting in the RFM69 receive buffer
+             */
+            if( ispacket )
             {
-
-/*
-#ifdef ENABLE_UART_OUTPUT
-                  rf69_lastRssi(&lastrssi);
-                  for (j=0; j<len; j++) {
-                  Serial.print((char)buf[j]);
-                  if(buf[j]==']') break;
-                  }
-                  Serial.print("|");
-                  Serial.println(rx_rssi);
-#endif
-*/
-
-                // Find end of packet & start of repeaters
-                uint8_t end_bracket = 255, start_bracket = 255;
-                for (int k=0; k<len; k++)
+                /* Find end of packet & start of repeaters */
+                end_bracket = 255;
+                start_bracket = 255;
+                /* TODO: This should be replaced with strstr() */
+                for (k = 0; k < len; k++)
                 {
                     if (buf[k] == '[')
                     {
@@ -257,49 +174,45 @@ void loop(void)
                     }
                 }
 
-                // Need to take the recieved buffer and decode it and 
-                // add a reference 
+                /* Need to take the recieved buffer and decode it and 
+                 * add a reference, under 3 conditions:
+                 * 1) The number of 'hops' remaining is > 0
+                 * 2) We found an end bracket in the packet, i.e. it is not
+                 * malformed in some way
+                 * 3) We have not /already/ repeated this packet
+                 * */
                 if (buf[0] > '0' && end_bracket != 255 && strstr((const char *)&buf[start_bracket], NODE_ID) == NULL)
                 {
-                    // Reduce the repeat value
+                    /* Reduce the TTL (hops) */
                     buf[0]--;
 
-                    // Add the repeater ID
+                    /* Add the repeater ID */
                     packet_len = end_bracket + sprintf((char *)&buf[end_bracket], ",%s]", NODE_ID);
-                    uint16_t delayValue = getRandBetween(5u, 80u);
-                    for (uint8_t j = 0; j < delayValue;j++)
-                    {
-                        //random delay to try and avoid packet collision
-                        _delay_ms(10);
-                    }
 
+                    /* Random delay to try and avoid packet collision */
+                    delaytime = getRandBetween(5u, 80u);
+                    for(j = 0; j < delaytime; j++)
+                        _delay_ms(10);
+
+                    /* Now send the repeated packet */
                     rf69_send((rfm_reg_t *)buf, packet_len, RFM_POWER);
-/*
-#ifdef ENABLE_UART_OUTPUT
-                      for (j=0; j<packet_len; j++) {
-                      if(buf[j]==']'){
-                      Serial.println((char)buf[j]);
-                      break;
-                      }
-                      Serial.print((char)buf[j]);
-                      }
-#endif
-*/
                 }
             }
         }
     }
     else
     {
-        // Battery Voltage Low - Zombie Mode, ignore all repeating
-        // functionality of the node
+        /* Battery Voltage Low - Zombie Mode, ignore all repeating
+         * functionality of the node */
 
         // Low Power Sleep for 8 seconds
         rf69_setMode(RFM69_MODE_SLEEP);
+
+        /* TODO: Low power sleep instead of busy waiting */
         _delay_ms(8000u);
     }
 
-    if (count >= data_interval)
+    if(count >= data_interval)
     {
         sequence_id++;
 
@@ -330,6 +243,87 @@ void loop(void)
         // Check if we need to enter or leave zombie mode
         zombieMode();
     }
+}
+
+/**
+ * Given a char buffer, generate a UKHASnet packet and return the length of
+ * the packet written to the supplied buffer.
+ * @param buf The char buffer into which to write the packet
+ * @returns The length of the packet that was written
+ */
+int16_t gen_data(char *buf)
+{
+    int8_t temp = 0;
+
+#ifdef LOCATION_STRING
+    if(sequence_id=='a' || sequence_id=='z') {
+        sprintf(buf, "%u%cL%s", NUM_REPEATS, sequence_id, LOCATION_STRING);
+    } else {
+        sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
+    }
+#else
+    sprintf(buf, "%u%c", NUM_REPEATS, sequence_id);
+#endif
+
+    rf69_readTemp(&temp);
+    sprintf(buf, "%sT%i.0", buf, temp);
+
+    // Battery Voltage
+#if ENABLE_BATTV_SENSOR == 1
+    battV = get_batt_voltage();
+    char* battStr;
+    char tempStrB[14]; //make buffer large enough for 7 digits
+    battStr = dtostrf(battV,7,2,tempStrB);
+    while( (strlen(battStr) > 0) && (battStr[0] == 32) )
+    {
+        strcpy(battStr,&battStr[1]);
+    }
+    sprintf(buf, "%sV%s", buf, battStr);
+#endif
+
+    // If zombie mode is enabled, put the current zombie status in telem
+#ifdef ENABLE_ZOMBIE_MODE
+    sprintf(buf, "%sZ%d", buf, zombie_mode);
+#endif /* ENABLE_ZOMBIE_MODE */
+
+    return sprintf(buf, "%s[%s]", buf, NODE_ID);
+}
+
+/**
+ * Measure the battery voltage as measured by a potential divider on BATTV_PIN
+ * defined in nodeconfig.h. BATTV_SCALEFACTOR should also be set appropriately
+ * in this file.
+ * @returns The voltage of the battery in Volts
+ */
+float get_batt_voltage(void)
+{
+    uint16_t res;
+
+    /* Power up the ADC */
+    PRR &= ~(_BV(PRADC));
+
+    /* Set channel to PC0 */
+    ADMUX = BATTV_PIN;
+
+    /* Set internal 1V1 reference */
+    ADMUX |= _BV(REFS1) | _BV(REFS0);
+
+    /* Get 125kHz ADC clock from a 1MHz core clock by dividing by 8 */
+    ADCSRA |= _BV(ADPS1) | _BV(ADPS0);
+
+    /* Enable ADC */
+    ADCSRA |= _BV(ADEN);
+
+    /* Start conv, wait until done, get result */
+    ADCSRA |= _BV(ADSC);
+    while(!(ADCSRA & _BV(ADIF)));
+    res = ADC;
+
+    /* Power down the ADC */
+    ADCSRA &= ~_BV(ADEN);
+    PRR |= _BV(PRADC);
+
+    return (float)(res * 0.001074219 * BATTV_SCALEFACTOR * BATTV_FUDGE);
 }
 
 /**
